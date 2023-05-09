8bdacb5bed1f1688d32bf48d848246d6d04fc675
LayneFongX <laynefongx@gmail.com>
2023-05-09 18:16:54 +0800

    提交代码

--- eventhub/src/main/java/com/farid/artemis/service/impl/EventHubsReceiveMsgService.java
+++ eventhub/src/main/java/com/farid/artemis/service/impl/EventHubsReceiveMsgService.java
@@ -1,11 +1,22 @@
 package com.farid.artemis.service.impl;
 
 
+import com.azure.messaging.eventhubs.EventData;
+import com.azure.messaging.eventhubs.EventHubClientBuilder;
+import com.azure.messaging.eventhubs.EventProcessorClient;
+import com.azure.messaging.eventhubs.EventProcessorClientBuilder;
+import com.azure.messaging.eventhubs.checkpointstore.blob.BlobCheckpointStore;
+import com.azure.messaging.eventhubs.models.ErrorContext;
+import com.azure.messaging.eventhubs.models.EventContext;
+import com.azure.messaging.eventhubs.models.PartitionContext;
+import com.azure.storage.blob.BlobContainerAsyncClient;
+import com.azure.storage.blob.BlobContainerClientBuilder;
 import com.farid.artemis.service.IEventHubsReceiveMsgService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.function.Consumer;
 
 /**
  * @Author Banchao
@@ -16,61 +27,59 @@ import java.io.IOException;
 @Service
 public class EventHubsReceiveMsgService implements IEventHubsReceiveMsgService {
 
-    // private final static String EVENT_HUB_CONNECTION = "Endpoint=sb://farid-event-hubs.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=moHMVM6lLekAbdi9y/TJLIX/yvKxQrynx+AEhDFqCwQ=";
-    //
-    // private final static String EVENT_HUB_NAME = "myeventhub";
-    //
-    // private static final String STORAGE_CONNECTION = "DefaultEndpointsProtocol=https;AccountName=fdstorageaccount;AccountKey=VFgne15epNUcs0ruPqbiMSNakmJgNS3BsEUYq1ZWYeAHUsBzlA9WCmlxwGUpPF1uxXJwCpnqR30B+AStxqt1Eg==;EndpointSuffix=core.windows.net";
-    //
-    // private static final String STORAGE_CONTAINER_NAME = "mycontainer";
+    private final static String EVENT_HUB_CONNECTION = "Endpoint=sb://farid-event-hubs.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=moHMVM6lLekAbdi9y/TJLIX/yvKxQrynx+AEhDFqCwQ=";
+
+    private final static String EVENT_HUB_NAME = "myeventhub";
+
+    private static final String STORAGE_CONNECTION = "DefaultEndpointsProtocol=https;AccountName=fdstorageaccount;AccountKey=VFgne15epNUcs0ruPqbiMSNakmJgNS3BsEUYq1ZWYeAHUsBzlA9WCmlxwGUpPF1uxXJwCpnqR30B+AStxqt1Eg==;EndpointSuffix=core.windows.net";
+
+    private static final String STORAGE_CONTAINER_NAME = "mycontainer";
+
+    public static final Consumer<EventContext> PARTITION_PROCESSOR = eventContext -> {
+        PartitionContext partitionContext = eventContext.getPartitionContext();
+        EventData eventData = eventContext.getEventData();
+
+        System.out.printf("Processing event from partition %s with sequence number %d with body: %s%n",
+                partitionContext.getPartitionId(), eventData.getSequenceNumber(), eventData.getBodyAsString());
+
+        if (eventData.getSequenceNumber() % 10 == 0) {
+            eventContext.updateCheckpoint();
+        }
+    };
+
+    public static final Consumer<ErrorContext> ERROR_HANDLER = errorContext -> {
+        System.out.printf("Error occurred in partition processor for partition %s, %s.%n",
+                errorContext.getPartitionContext().getPartitionId(),
+                errorContext.getThrowable());
+    };
 
     @Override
     public void receiveMessages() throws IOException {
-        // // Create a blob container client that you use later to build an event processor client to receive and process events
-        // BlobContainerAsyncClient blobContainerAsyncClient = new BlobContainerClientBuilder()
-        //         .connectionString(STORAGE_CONNECTION)
-        //         .containerName(STORAGE_CONTAINER_NAME)
-        //         .buildAsyncClient();
-        //
-        // // Create a builder object that you will use later to build an event processor client to receive and process events and errors.
-        // EventProcessorClientBuilder eventProcessorClientBuilder = new EventProcessorClientBuilder()
-        //         .connectionString(EVENT_HUB_CONNECTION, EVENT_HUB_NAME)
-        //         .consumerGroup(EventHubClientBuilder.DEFAULT_CONSUMER_GROUP_NAME)
-        //         .processEvent(PARTITION_PROCESSOR)
-        //         .processError(ERROR_HANDLER)
-        //         .checkpointStore(new BlobCheckpointStore(blobContainerAsyncClient));
-        //
-        // // Use the builder object to create an event processor client
-        // EventProcessorClient eventProcessorClient = eventProcessorClientBuilder.buildEventProcessorClient();
-        //
-        // System.out.println("Starting event processor");
-        // eventProcessorClient.start();
-        //
-        // System.out.println("Press enter to stop.");
-        // System.in.read();
-        //
-        // System.out.println("Stopping event processor");
-        // eventProcessorClient.stop();
-        // System.out.println("Event processor stopped.");
-        //
-        // System.out.println("Exiting process");
-    }
+        BlobContainerAsyncClient blobContainerAsyncClient = new BlobContainerClientBuilder()
+                .connectionString(STORAGE_CONNECTION)
+                .containerName(STORAGE_CONTAINER_NAME)
+                .buildAsyncClient();
 
-    // public static final Consumer<EventContext> PARTITION_PROCESSOR = eventContext -> {
-    //     PartitionContext partitionContext = eventContext.getPartitionContext();
-    //     EventData eventData = eventContext.getEventData();
-    //
-    //     System.out.printf("Processing event from partition %s with sequence number %d with body: %s%n",
-    //             partitionContext.getPartitionId(), eventData.getSequenceNumber(), eventData.getBodyAsString());
-    //
-    //     if (eventData.getSequenceNumber() % 10 == 0) {
-    //         eventContext.updateCheckpoint();
-    //     }
-    // };
-    //
-    // public static final Consumer<ErrorContext> ERROR_HANDLER = errorContext -> {
-    //     System.out.printf("Error occurred in partition processor for partition %s, %s.%n",
-    //             errorContext.getPartitionContext().getPartitionId(),
-    //             errorContext.getThrowable());
-    // };
+        EventProcessorClientBuilder eventProcessorClientBuilder = new EventProcessorClientBuilder()
+                .connectionString(EVENT_HUB_CONNECTION, EVENT_HUB_NAME)
+                .consumerGroup(EventHubClientBuilder.DEFAULT_CONSUMER_GROUP_NAME)
+                .processEvent(PARTITION_PROCESSOR)
+                .processError(ERROR_HANDLER)
+                .checkpointStore(new BlobCheckpointStore(blobContainerAsyncClient));
+
+        // Use the builder object to create an event processor client
+        EventProcessorClient eventProcessorClient = eventProcessorClientBuilder.buildEventProcessorClient();
+
+        System.out.println("Starting event processor");
+        eventProcessorClient.start();
+
+        System.out.println("Press enter to stop.");
+        System.in.read();
+
+        System.out.println("Stopping event processor");
+        eventProcessorClient.stop();
+        System.out.println("Event processor stopped.");
+
+        System.out.println("Exiting process");
+    }
 }
--- eventhub/src/main/java/com/farid/artemis/service/impl/EventHubsSendMsgService.java
+++ eventhub/src/main/java/com/farid/artemis/service/impl/EventHubsSendMsgService.java
@@ -1,8 +1,15 @@
 package com.farid.artemis.service.impl;
 
+import com.azure.messaging.eventhubs.EventData;
+import com.azure.messaging.eventhubs.EventDataBatch;
+import com.azure.messaging.eventhubs.EventHubClientBuilder;
+import com.azure.messaging.eventhubs.EventHubProducerClient;
 import com.farid.artemis.service.IEventHubsSendMsgService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @Author Banchao
@@ -13,31 +20,31 @@ import org.springframework.stereotype.Service;
 @Service
 public class EventHubsSendMsgService implements IEventHubsSendMsgService {
 
-    // private final static String EVENT_HUB_CONNECTION = "Endpoint=sb://farid-event-hubs.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=moHMVM6lLekAbdi9y/TJLIX/yvKxQrynx+AEhDFqCwQ=";
-    //
-    // private final static String EVENT_HUB_NAME = "myeventhub";
+    private final static String EVENT_HUB_CONNECTION = "Endpoint=sb://farid-event-hubs.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=moHMVM6lLekAbdi9y/TJLIX/yvKxQrynx+AEhDFqCwQ=";
+
+    private final static String EVENT_HUB_NAME = "myeventhub";
 
     @Override
     public void sendEventHubsMsg() {
-        // EventHubProducerClient producer = new EventHubClientBuilder().connectionString(EVENT_HUB_CONNECTION, EVENT_HUB_NAME).buildProducerClient();
-        //
-        // List<EventData> allEvents = Arrays.asList(new EventData("Foo"), new EventData("Bar"));
-        //
-        // EventDataBatch eventDataBatch = producer.createBatch();
-        // for (EventData eventData : allEvents) {
-        //     if (!eventDataBatch.tryAdd(eventData)) {
-        //         producer.send(eventDataBatch);
-        //         eventDataBatch = producer.createBatch();
-        //
-        //         if (!eventDataBatch.tryAdd(eventData)) {
-        //             throw new IllegalArgumentException("Event is too large for an empty batch. Max size: "
-        //                     + eventDataBatch.getMaxSizeInBytes());
-        //         }
-        //     }
-        // }
-        // if (eventDataBatch.getCount() > 0) {
-        //     producer.send(eventDataBatch);
-        // }
-        // producer.close();
+        EventHubProducerClient producer = new EventHubClientBuilder().connectionString(EVENT_HUB_CONNECTION, EVENT_HUB_NAME).buildProducerClient();
+
+        List<EventData> allEvents = Arrays.asList(new EventData("Foo"), new EventData("Bar"));
+
+        EventDataBatch eventDataBatch = producer.createBatch();
+        for (EventData eventData : allEvents) {
+            if (!eventDataBatch.tryAdd(eventData)) {
+                producer.send(eventDataBatch);
+                eventDataBatch = producer.createBatch();
+
+                if (!eventDataBatch.tryAdd(eventData)) {
+                    throw new IllegalArgumentException("Event is too large for an empty batch. Max size: "
+                            + eventDataBatch.getMaxSizeInBytes());
+                }
+            }
+        }
+        if (eventDataBatch.getCount() > 0) {
+            producer.send(eventDataBatch);
+        }
+        producer.close();
     }
 }
