72f1eb3591da21e6e0a821e907e09d2a8dc8a529
LayneFongX <laynefongx@gmail.com>
2023-05-09 15:03:37 +0800

    提交代码

--- eventhub/src/main/java/com/farid/artemis/service/impl/EventHubsSendMsgService.java
+++ eventhub/src/main/java/com/farid/artemis/service/impl/EventHubsSendMsgService.java
@@ -20,31 +20,31 @@ import java.util.List;
 @Service
 public class EventHubsSendMsgService implements IEventHubsSendMsgService {
 
-    private final static String EVENT_HUB_CONNECTION = "Endpoint=sb://farid-event-hubs.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=moHMVM6lLekAbdi9y/TJLIX/yvKxQrynx+AEhDFqCwQ=";
-
-    private final static String EVENT_HUB_NAME = "myeventhub";
+    // private final static String EVENT_HUB_CONNECTION = "Endpoint=sb://farid-event-hubs.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=moHMVM6lLekAbdi9y/TJLIX/yvKxQrynx+AEhDFqCwQ=";
+    //
+    // private final static String EVENT_HUB_NAME = "myeventhub";
 
     @Override
     public void sendEventHubsMsg() {
-        EventHubProducerClient producer = new EventHubClientBuilder().connectionString(EVENT_HUB_CONNECTION, EVENT_HUB_NAME).buildProducerClient();
-
-        List<EventData> allEvents = Arrays.asList(new EventData("Foo"), new EventData("Bar"));
-
-        EventDataBatch eventDataBatch = producer.createBatch();
-        for (EventData eventData : allEvents) {
-            if (!eventDataBatch.tryAdd(eventData)) {
-                producer.send(eventDataBatch);
-                eventDataBatch = producer.createBatch();
-
-                if (!eventDataBatch.tryAdd(eventData)) {
-                    throw new IllegalArgumentException("Event is too large for an empty batch. Max size: "
-                            + eventDataBatch.getMaxSizeInBytes());
-                }
-            }
-        }
-        if (eventDataBatch.getCount() > 0) {
-            producer.send(eventDataBatch);
-        }
-        producer.close();
+        // EventHubProducerClient producer = new EventHubClientBuilder().connectionString(EVENT_HUB_CONNECTION, EVENT_HUB_NAME).buildProducerClient();
+        //
+        // List<EventData> allEvents = Arrays.asList(new EventData("Foo"), new EventData("Bar"));
+        //
+        // EventDataBatch eventDataBatch = producer.createBatch();
+        // for (EventData eventData : allEvents) {
+        //     if (!eventDataBatch.tryAdd(eventData)) {
+        //         producer.send(eventDataBatch);
+        //         eventDataBatch = producer.createBatch();
+        //
+        //         if (!eventDataBatch.tryAdd(eventData)) {
+        //             throw new IllegalArgumentException("Event is too large for an empty batch. Max size: "
+        //                     + eventDataBatch.getMaxSizeInBytes());
+        //         }
+        //     }
+        // }
+        // if (eventDataBatch.getCount() > 0) {
+        //     producer.send(eventDataBatch);
+        // }
+        // producer.close();
     }
 }
